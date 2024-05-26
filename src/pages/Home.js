ecd355f04afdad0fa1d323f4c8cfa3db5a7bdbbf
samir2398 <samiransary2002@gmail.com>
2024-05-26 16:15:43 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedin}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      Home
+    </div>
+  )
+}
+
+export default Home
